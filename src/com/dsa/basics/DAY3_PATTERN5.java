cb5c2dc773d750eed94ae17fe22442ea13ef8e36
psthejohn <psthejohn@gmail.com>
2025-05-07 11:51:50 +0530

    ✅ char pattern Added

---
+++ src/com/dsa/basics/DAY3_PATTERN5.java
@@ -0,0 +1,13 @@
+public class DAY3_PATTERN5 {
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    private static void pattern(int n){
+        for(int i=0; i<n; i++){
+            for(char j='A'; j<='A'+i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
